140533b35f7aaf2b54836cfdc83568e89d08ab62
Ryan Ford <fordryan01@hotmail.co.uk>
2025-05-30 17:09:25 +0100

    Adding the game variables that are needed to the JS file and adding the startGame and openRules function that current buttons will call upon

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//Game Variables
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    turnNumber: 0,
+    lives: 3,
+    lastNumber: "",
+    allowInput: false,
+    endGame: false,
+    choices: ["button1", "button2", "button3", "button4", "button5", "button6", "button7", "button8", "button9"],
+}
+
+function startGame(){
+}
+
+function openRules(){
+    
+}
