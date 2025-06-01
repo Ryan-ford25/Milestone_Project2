88b5f65c227434099d16e81ec9f09d1b14c68603
Ryan Ford <fordryan01@hotmail.co.uk>
2025-06-01 11:02:21 +0100

    Adding in comments at each line of code in the javascript file to explain how it works

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,28 +28,28 @@ function startGame(){
     newGame();
 }
 
-
+//Starts a new game resetting all game variables to their default values
 function newGame(){
-    game.lives = 3;//Sets the game variables at the start of every game.
+    game.lives = 3;
     game.score = 0;
     game.playermoves = [];
     game.currentGame= [];
     for (let gridSquare of document.getElementsByClassName("grid_square")) {
-        if (gridSquare.getAttribute("data-listener") !== "true"){//Checks if it has a data listener of false
-            gridSquare.addEventListener("click", (e) => {//Adds a click event listener to the square
-                if(game.currentGame.length > 0 && !game.turnInProgress) { //Checks if the length is more than 0 and if input is set to true
-                    let move = e.target.getAttribute("id"); //Gets the id of the clicked square then stores it in the variable move
-                    game.lastButton = move; //Stores the move within the last button variable 
-                    lightUp(move); //Lights up the square that has been clicked
-                    game.playerMoves.push(move); //Adds the 'move' to the player moves variable
-                    playerTurn(); //Runs the players move function
+        if (gridSquare.getAttribute("data-listener") !== "true"){             //Checks if it has a data listener of false
+            gridSquare.addEventListener("click", (e) => {                     //Adds a click event listener to the square
+                if(game.currentGame.length > 0 && !game.turnInProgress) {     //Checks if the length is more than 0 and if input is set to true
+                    let move = e.target.getAttribute("id");                   //Gets the id of the clicked square then stores it in the variable move
+                    game.lastButton = move;                                   //Stores the move within the last button variable 
+                    lightUp(move);                                            //runs the lightUp function with the value move
+                    game.playerMoves.push(move);                              //Adds the 'move' to the player moves variable
+                    playerTurn();                                             //Runs the players move function
                 }
             });
-            gridSquare.setAttribute("data-listener", "true"); //Sets the squares data listener to true
+            gridSquare.setAttribute("data-listener", "true");                 //Sets the squares data listener to true
         }
     }
     showLives();
-    showScore();//Runs the corresponding function.
+    showScore();                                                              //Runs the corresponding function.
     addTurn();
 }
 
@@ -60,71 +60,80 @@ function addTurn(){
     showTurns();
 }
 
-function showScore(){ //Displays the players score
+//ShowScore function takes the value of variable score and displays it with the message 'Score: '
+function showScore(){
     document.getElementById("score").innerText = `Score: ${game.score}`
 }
 
+//ShowLives function takes the value of the variable lives and displays it with the message 'Lives: '
 function showLives(){ //Displays the players lives
     document.getElementById("lives").innerText = `Lives: ${game.lives}`
 }
 
-function lightUp(sqr){ //Adds the class 'lit' to relevent square then removes the class after a 300 milliseconds delay
+//Adds the class 'lit' from the css file to the chosen square in order to "light" the square up (changes opacity)
+function lightUp(sqr){ 
     document.getElementById(sqr).classList.add("lit");
     setTimeout(() => {
         document.getElementById(sqr).classList.remove("lit");
     }, 400);
 }
 
+
+//ShowTurns function displays the sequence to the player
 function showTurns(){
-    game.turnInProgress = true;
-    game.turnNumber = 0;
-    if (game.turnInterval !== null){
-        clearInterval(game.turnInterval);
+    game.turnInProgress = true;                                   //Sets the turnInProgress variable to true, which will prevent player input
+    game.turnNumber = 0;                                         //Sets the turnNumber variable to 0
+    if (game.turnInterval !== null){                            //If statement resets any current turnIntervals in order to prevent overlapping(doing this fixed an error with the sequence being played too fast after selecting start from the end screen)
+        clearInterval(game.turnInterval);                               
     }
-    game.turnInterval = setInterval(() => {
+    game.turnInterval = setInterval(() => {                     //Sets a new interval to display the sequence with
         lightUp(game.currentGame[game.turnNumber]);
-        game.turnNumber++;
-        if (game.turnNumber >= game.currentGame.length){
-            clearInterval(game.turnInterval);
-            game.turnInterval = null;
-            game.turnInProgress = false;
+        game.turnNumber++;                                       //increases the value of the variable turnNumber by 1 to add a square to the current sequence
+        if (game.turnNumber >= game.currentGame.length){        //if statement checks if the turnNumber variable is greater than or equal to the current games length and then stops the interval
+            clearInterval(game.turnInterval);                   
+            game.turnInterval = null;                           //sets the value of variable turnInterval to null
+            game.turnInProgress = false;                        //sets value of turnInProgress to false to allow user input
         }
     }, 800);
 }
 
+//playerTurn function takes the users input and checks if it matches the sequence the computer generated
 function playerTurn(){
-    let n = game.playerMoves.length -1;
-    if (game.currentGame[n] === game.playerMoves[n]){
-        if (game.currentGame.length === game.playerMoves.length){
-            game.score++;
-            showLives();
-            showScore();
-            addTurn();
+    let n = game.playerMoves.length -1;                             //stores the index of the players last move to n
+    if (game.currentGame[n] === game.playerMoves[n]){               //Checks if the players chosen squares matches the ones that were generated by the computer 
+        if (game.currentGame.length === game.playerMoves.length){   //Checks if the length of the current game and the players input match
+            game.score++;                                           //If they match the score is increased by 1
+            showLives();                                            //Runs the showLives function
+            showScore();                                            //Runs showScore function
+            addTurn();                                              //Runs addTurn function
         }
-    } else{
-        game.lives = game.lives -1;
-        showLives()
-        if (game.lives == 0){
-            endGame();
+    } else{                                                         //If the moves and length does not match then the following steps are carried out instead
+        game.lives = game.lives -1;                                 //Decreases the value of the variable lives by 1
+        showLives()                                                 //Runs the showLives function(displaying the newly updated value)
+        if (game.lives == 0){                                       //Checks if the value of lives is equal to 0
+            endGame();                                              //If it is then the endGame function is ran
         } else {
-            game.playerMoves=[];
-            setTimeout(() => {
-                showTurns();
+            game.playerMoves=[];                                    //If it is not equal to 0, the playerMoves variable is reset
+            setTimeout(() => {                                      //Uses the timeout function in order to add an 800 millisecond delay
+                showTurns();                                        //Runs the showTurns function which will repeat the last sequence 
             }, 800);
         }
     }
 }
 
+//endScore function displays the players score within the message 'You made it to level: '
 function endScore(){
     document.getElementById("end_score").innerText = `You made it to Level: ${game.score}`
 }
 
+
+//endGame function displays a message after the lives have reached 0 and then waits for an input from the user 
 function endGame() {
-    $("#game_screen").addClass("hidden");
-    $("#end_screen").removeClass("hidden");
-    endScore();
-    $("#end_screen").on("click", function(){
-        $("#end_screen").addClass("hidden");
-        $("#welcome_screen").removeClass("hidden");
+    $("#game_screen").addClass("hidden");             //Adds the css class 'hidden' to the game screen in order to hide it 
+    $("#end_screen").removeClass("hidden");            //Removes the class 'hidden' to display the end screen message
+    endScore();                                       //Runs the endScore function to display the level reached
+    $("#end_screen").on("click", function(){          //users a click listener to wait for input from the user
+        $("#end_screen").addClass("hidden");           //When the user clicks on end screen the 'hidden' class will be added to the end screen to hide it
+        $("#welcome_screen").removeClass("hidden");    //Removes the class 'hidden' from welcome screen to display it again allowing the user to restart
     })
 }
